825b6c0396841f9256871a11ac2250381353557d
Sebastian Kopf <skopf@caltech.edu>
2014-04-06 22:27:15 -0700

    plotting and data table implemented
     - doubling times selection
     - error messages
     - plot and table export

--- isolabel/server.R
+++ isolabel/server.R
@@ -4,36 +4,10 @@ library(stringr)
 library(datasets)
 library(ggplot2)
 library(grid)
-
-#' Duration labels to the closest large denominator (e.g. 86400s = ~24 hours)
-#'@param ds vector of lubridate:::duration objects or time in seconds
-duration_label <- function(ds) {
-    sapply(ds, function(x) { 
-        if (grepl("^[0-9\\.]+s$", (d <- duration(x, "seconds")))) return(paste0('~', round(as.numeric(d), 1), " seconds"))
-        else return(str_extract(d, "~.[^\\)]*"))
-    })
-}
-
 library(plyr)
-#' Labeling time as function of doubling time, desired isotopic enrichment and strength of label
-#'@param dblt doubling times, a vector of lubridate:::duration objects
-#'@param Ex_sample - sample enrichment (in permil)
-#'@param F_label - labeling strength (in fractional abundance)
-#'@param R_nat - natural abundance of the labeled material (isotope ratio)
-#'@return data frame with input columns and additional columns label.time, alpha and s
-label_time <- function(dblt, Ex_sample, F_label, R_nat) {
-    df <- expand.grid(dblt = dblt, Ex_sample = Ex_sample, F_label = F_label, R_nat = R_nat, stringsAsFactors = FALSE)
-    mutate(df, 
-           dblt.label = duration_label(dblt), # add labels
-           alpha = iso.ExtoA(Ex_sample), # convert epsilon to alpha
-           s = F_label / iso.RtoF(R_nat), # convert label to s
-           labeling_time = dblt * 1/log(2) * log((1 - s) / (alpha - s)), # calculate label time
-           labeling_time.label = duration_label(labeling_time)) # label
-}
+library(reshape2)
 
 # isotope functions
-iso.NtoR<-function(NM, Nm) Nm/NM
-iso.NtoF<-function(NM, Nm) Nm/(NM + Nm)
 iso.RtoF<-function(ratio) ratio/(1+ratio) # ratio to fraction
 iso.FtoR<-function(frac) frac/(1-frac) # fraction to ratio
 iso.RtoD<-function(ratios, refs) ratios/refs - 1 #fraction to delta value (NOT 1000x!!)
@@ -50,56 +24,168 @@ iso.AtoEx<-function(alphas) (alphas-1)*1000 #alpha to epsilon (as permill, i.e. 
 iso.ExtoA<-function(eps) eps/1000 + 1 #epsilon in permil to alpha
 iso.DxtoEx<-function(deltas1, deltas2) iso.AtoEx(iso.DxtoA(deltas1, deltas2)) # from delta notation (in permil, i.e. 1000x) to epsilon in permil
 
+#' Constants used for isotopic reference values
+c.R_nat <<- mutate(data.frame(
+    minor = c("2H", "13C", "15N", "18O", "34S"),
+    major = c("1H", "12C", "14N", "16O", "32S"),
+    ratio = c(0.00015575, 0.011237, 0.003677, 0.0020052, 0.0045005),
+    ref = c("VSMOW", "VPDB", "Air", "VSMOW", "CDT"),
+    stringsAsFactors=F),
+    abundance = iso.RtoF(ratio),
+    label = paste0(minor, "/", major))
+
+#' Duration labels to the closest large denominator (e.g. 86400s = ~24 hours)
+#'@param ds vector of lubridate:::duration objects or time in seconds
+duration_label <- function(ds) {
+    sapply(ds, function(x) { 
+        if (grepl("^[0-9\\.]+s$", (d <- duration(x, "seconds")))) return(paste0('~', round(as.numeric(d), 1), " seconds"))
+        else return(str_extract(d, "~.[^\\)]*"))
+    })
+}
+
+#' Labeling time as function of doubling time, desired isotopic enrichment and strength of label
+#' @param dblt doubling times, a vector of lubridate:::duration objects
+#' @param Ex_sample sample enrichment (in permil)
+#' @param F_label labeling strength (in fractional abundance)
+#' @param R_nat natural abundance of the labeled material (isotope ratio)
+#' @return data frame with input columns and additional columns label.time, alpha and s
+label_time <- function(dblt, Ex_sample, F_label, R_nat) {
+    df <- expand.grid(dblt = dblt, Ex_sample = Ex_sample, F_label = F_label, R_nat = R_nat, stringsAsFactors = FALSE)
+    mutate(df, 
+           dblt.label = duration_label(dblt), # add labels
+           alpha = iso.ExtoA(Ex_sample), # convert epsilon to alpha
+           s = F_label / iso.RtoF(R_nat), # convert label to s (labeling strenth relative to natural abundance)
+           labeling_time = as.numeric(dblt) * 1/log(2) * log((1 - s) / (alpha - s)), # calculate label time
+           labeling_time.label = duration_label(labeling_time)) # label
+}
+
+#' Isotopic enrichment as a function of labeling time, doubling time and strength of label
+#' @param time labeling times, a vector of lubridate:::duration objects
+#' @param dblt doubling times, a vector of lubridate:::duration objects
+#' @param F_label labeling strength (in fractional abundance)
+#' @param R_nat natural abundance of the labeled material (isotope ratio)
+label_strength <- function(time, dblt, F_label, R_nat) {
+    df <- expand.grid(time = time, dblt = dblt, F_label = F_label, R_nat = R_nat, stringsAsFactors = FALSE)
+    mutate(df,
+           time.label = duration_label(time), # add labels
+           dblt.label = duration_label(dblt), # add labels
+           p = log(2)/as.numeric(dblt), # specific growth rate
+           nat.abundance = iso.RtoF(R_nat), # natural abundance
+           enrich.abundance = F_label - (F_label - nat.abundance) * exp(-p * as.numeric(time)), # predicted isotopic composition
+           enrich.permil = iso.FtoDx(enrich.abundance, R_nat) # enrichment in permil
+           )
+}
+
 # Define server logic required to generate and plot a random distribution
-shinyServer(function(input, output) {
+shinyServer(function(input, output, session) {
     
-    datasetInput <- reactive({
-        # dates to plot
-        dblts <- list(c(1, "hour"), c(1, "day"), c(1, "month"), c(1, "year"), c(10, "years"), c(100, "years"))
+    # Inputs processing ------------------------------------------------
     
-        c.RDnat <- 0.00015575 # D/H of VSMOW
-        c.R15Nnat <- 0.003677 # 15N/14N of Air
-        c.R13Cnat <- 0.011237 # 13C/12C of VPDB
-        c.R18Onat <- 0.0020052 # 18O/16O of VSMOW
-        c.R34Snat <- 0.0045005 # 34S/32S of CDT
-        refs <- c("2H" = c.RDnat, "13C" = c.R13Cnat, "15N" = c.R15Nnat, "18O" = c.R18Onat, "34S" = c.R34Snat)
+    # doubling times
+    dbltsInput <- reactive({
+        # find doubling times to plot
+        dblts <- c()
+        dblts.labels <- c()
+        for (i in 1:5) { 
+            if (paste0("dblt", i) %in% names(input)) {
+                value <- input[[paste0("dblt", i)]]
+                unit <- paste0(c(input[[paste0("dblt", i, "_units")]]), if (value > 1) "s")
+                dblt <- duration(as.integer(value), unit)
+                if (! (dblt %in% dblts) ) { # only take unique dblts
+                    dblts <- c(dblts, dblt)
+                    dblts.labels <- c(dblts.labels, paste(value, unit))
+                }
+            } else 
+                break
+        }
+        list(dblts = dblts, dblts.labels = dblts.labels)
+    })
+    
+    # labeling strengths
+    labelsInput <- reactive({
+        data <- list()
         
-        # enrichment
+        # natural abundance reference 
+        data$nat <- subset(c.R_nat, minor == input$ref)
+        
+        # target enrichment
         if (input$targetType == 'permil')
-            enrich <- input$intensity_permil
+            data$enrichPermil <- input$intensity_permil
         else 
-            enrich <- iso.FtoDx(input$intensity_F, refs[[input$ref]])
+            data$enrichPermil <- iso.FtoDx(input$intensity_F, data$nat$ratio)
+        data$enrichF <- iso.DxtoF(data$enrichPermil, data$nat$ratio)
         
         # labels
-        labels <- c(input$label1, input$label2, input$label3)
-        labels <- labels[as.integer(input$show)]
+        data$labels <- sapply(1:3, function(i) input[[paste0("label", i)]], simplify = T)
+        data$labels.show <- 1:3 %in% as.integer(input$show)
+        data$labels.error <- (data$labels <= data$enrichF)
         
-        # FIXME: implement check that enrichment is possible with the given label and exclude labels that are not possible
+        return(data)
+    })
+    
+    # dataset 
+    datasetInput <- reactive({
+        dblts <- dbltsInput()
+        data <- labelsInput()
         
-        # labeling times
+        # labels
+        show <- (which(data$labels.show & !data$labels.error))
+        
+        # errors
+        if (length(show) == 0)
+            stop("Please enable at least one valid labeling strength.")
+        if (data$enrichF <= data$nat$abundance)
+            stop("Please choose a higher target enrichment.")
+        
+        # labeling times for plot
         plot.df <- label_time(
-            dblt = sapply(dblts, function(x) duration(as.integer(x[1]), x[2])), 
-            Ex_sample = round(enrich), 
-            F_label = labels, 
-            R_nat = refs[[input$ref]])
+            dblt = dblts$dblts, 
+            Ex_sample = round(data$enrichPermil), 
+            F_label = data$labels[show], 
+            R_nat = data$nat$ratio)
         plot.df$Label <- factor(paste0(100*plot.df$F_label, "%")) # legend in %
         
-        return(plot.df)
-    })
- 
-    # The output$summary depends on the datasetInput reactive expression, 
-    # so will be re-executed whenever datasetInput is invalidated
-    # (i.e. whenever the input$dataset changes)
-    output$summary <- renderPrint({
-        dataset <- datasetInput()
-        summary(dataset)
+        # enrichment for table
+        table.df <- data.frame()
+        for (label in data$labels[show]) {
+            df <- subset(plot.df, F_label == label)
+            table.df <- rbind(table.df, label_strength(
+                time = duration(df$labeling_time, "seconds"), 
+                dblt = duration(df$dblt, "seconds"), 
+                F_label = unique(df$F_label),
+                R_nat = unique(df$R_nat))
+            )
+        }
+        
+        # merge with labels
+        table.df <- merge(table.df, data.frame(dblt = dblts$dblts, dblt.userlabel = paste0(dblts$dblts.labels, " doubling")), by = 'dblt')
+        
+        # adjust headers
+        table.df <- mutate(
+            table.df, 
+            `Label` = paste0(100*F_label, "%"),
+            `Incubation time [s]` = as.numeric(time), 
+            `Incubation time` = time.label)
+        
+        if (input$tableDataType == "permil") {
+            table.df$enrichment <- paste0(signif(table.df$enrich.permil, 2))
+        } else {
+            table.df$enrichment <- paste0(signif(100*table.df$enrich.abundance, 2))
+        }
+        
+        # cast 
+        table.df <- dcast(table.df, `Label` + `Incubation time [s]` + `Incubation time` ~ dblt.userlabel, value.var = "enrichment" )
+        header_order <- match(paste0(dblts$dblts.labels, " doubling"), names(table.df))
+        table.df <- table.df[c(1,3,header_order)]
+        
+        return(c(
+            dblts, data, list(plot.df = plot.df, table.df = table.df)))
     })
     
-    
-    output$plot <- renderPlot({
-        plot.df <- datasetInput()
-        dblts <- list(c(1, "hour"), c(1, "day"), c(1, "month"), c(1, "year"), c(10, "years"), c(100, "years"))
-        p <- ggplot(plot.df,
+    # plot
+    plotInput <- reactive({
+        data <- datasetInput()
+        ggplot(data$plot.df,
                aes(x = dblt, y = labeling_time, fill = Label, colour = Label)) + 
             geom_segment(aes(x=0, xend=dblt, y=labeling_time, yend=labeling_time), 
                          arrow=arrow(length=unit(0.4,"cm"), ends="first", type="open", 
@@ -109,16 +195,86 @@ shinyServer(function(input, output) {
                          linetype=2) + 
             geom_line(linetype=1) + geom_point(colour="black", shape = 21, size=4) +
             scale_y_log10(
-                expand = c(0, 0.1), breaks = unique(plot.df$labeling_time), 
-                labels = unique(plot.df$labeling_time.label)) + 
-            scale_x_log10(expand = c(0, 0.2), breaks = unique(plot.df$dblt), 
-                          labels = sapply(dblts, function(x) paste(x[1], x[2]))) + 
-            labs(title = paste0("Required labeling times for ", round(unique(plot.df$Ex_sample)), 
-                                " permil enrichment (", 
-                                round(iso.DxtoF(unique(plot.df$Ex_sample), unique(plot.df$R_nat)) * 100, 2),
+                expand = c(0, 0.1), breaks = unique(data$plot.df$labeling_time), 
+                labels = unique(data$plot.df$labeling_time.label)) + 
+            scale_x_log10(expand = c(0, 0.2), breaks = data$dblts, 
+                          labels = data$dblts.labels) + 
+            labs(title = paste0("Required labeling times for ", round(data$enrichPermil), 
+                                " permil enrichment (", signif(data$enrichF * 100, 3),
                                 "% ", input$ref, ")\n"), x = "Doubling time", y = "") + 
             theme_bw() + theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank()) + 
             theme(legend.position = "right")
-        suppressWarnings(print(p))
     })
+    
+    
+    # Outputs rendering ---------------------------------------------
+    
+    # natural abundance info output
+    output$nat <- renderUI({ 
+        nat <- subset(c.R_nat, minor == input$ref)
+        paste0("Reference ", nat$label, " ratio: ", nat$ratio, " (", nat$ref, ")")
+    })
+    
+    # labeling strengths messages output
+    generate_label_msg <- function(i) {
+        renderUI({
+            data <- labelsInput()
+            if (data$labels.error[i]) {
+                return (paste0("Too small, can't possibly reach target enrichment (", signif(data$enrichF * 100, 3), "%)."))
+            } else
+                return ("")
+        })
+    }
+    output$label1_message <- generate_label_msg(1)
+    output$label2_message <- generate_label_msg(2)
+    output$label3_message <- generate_label_msg(3)   
+    
+    # enrichment info output
+    output$intensity_F_message <- renderUI({ 
+        data <- labelsInput()
+        if (data$enrichF <= data$nat$abundance)
+            return(paste0("Too small, target enrichment must exceed natural abundance."))
+        else
+            return ("")
+    })
+    
+    # rendering plot
+    output$plot <- renderPlot({
+        withProgress(session, min=1, max=5, {
+            setProgress(message = 'Rendering plot ...')
+            setProgress(value = 2)
+            p <- plotInput()
+            setProgress(value = 4)
+            suppressWarnings(print(p))
+            setProgress(value = 5)
+        })
+    })
+    
+    # saving plot
+    output$downloadPlot <- downloadHandler(
+        filename = function() { paste0('isotope_labeling_', input$ref, '.pdf') },
+        content = function(file) {
+            device <- function(..., version="1.4")
+                grDevices::pdf(..., version=version)
+            ggsave(file, plot = plotInput(), device = device)
+        }
+    )
+    
+    # rendering table
+    output$table <- renderTable({
+        withProgress(session, min=1, max=5, {    
+            setProgress(message = 'Rendering table ...')
+            setProgress(value = 2)
+            data.table <- datasetInput()$table.df
+            setProgress(value = 4)
+            return(data.table)
+        })
+    }, digits = 0, include.rownames = F, align = rep('r', ncol(datasetInput()$table.df) + 1))
+    
+    # saving table 
+    output$downloadTable <- downloadHandler(
+        filename = function() { paste0('isotope_labeling_', input$ref, '.csv') },
+        content = function(file) { write.csv(datasetInput()$table.df, file) }
+    )
+    
 })
--- isolabel/ui.R
+++ isolabel/ui.R
@@ -1,71 +1,112 @@
 library(shiny)
+library(shinyIncubator)
 
-# Define UI for application that plots random distributions 
-shinyUI(pageWithSidebar(
-    
-    # Application title
-    headerPanel("Isotope labeling calculator"),
-    
-    # Sidebar with a slider input for number of observations
-    sidebarPanel(
-        selectInput("ref", "Choose an isotope system:", 
-                    choices = c("2H", "13C", "15N", "18O", "34S")),
-        sliderInput("label1", 
-                    "Isotope label strengths:", 
-                    min = 0,
-                    max = 1,
-                    step = 0.001,
-                    value = 0.01, 
-                    format="#.#%"),
-        sliderInput("label2", 
-                    "", 
-                    min = 0,
-                    max = 1,
-                    step = 0.001,
-                    value = 0.05, 
-                    format="#.#%"),
-        sliderInput("label3", 
-                    "", 
-                    min = 0,
-                    max = 1,
-                    step = 0.001,
-                    value = 0.15, 
-                    format="#.#%"),
-        checkboxGroupInput(inputId="show", label="Show labeleling strengths:", choices=1:3, selected=1:3),
-        tags$style(type="text/css", HTML("#show>*{float: left; margin-right: 15px; height: 20px;} #show {height: 20px;}")),
+# doubling time picking
+dblt_picker <- function(i, value = 1, unit = c("minute", "hour", "day", "week", "month", "year")) {
+    unit <- match.arg(unit)
+    units <- eval(formals(dblt_picker)$unit, list())
+    fluidRow(
+        column(3, numericInput(paste0("dblt", i), "", value, min = 1, step = 1)),
+        column(6, offset = 1, selectInput(paste0("dblt", i, "_units"), "", choices = units, selected = unit))
+    )
+}
+
+# Define UI that plots the isotope label enrichment 
+shinyUI(
+    fluidPage(
         
-        selectInput(
-            "targetType", "Target enrichment:",
-            c(`Relative enrichment [in permil]` = "permil",
-              `Total fractional abundance [% light isotope]` = "frac"), selected="permil"),
-        
-        conditionalPanel(
-            condition = "input.targetType == 'permil'",
-            sliderInput("intensity_permil", 
-                        "", 
-                        min = 100,
-                        max = 50000,
-                        step = 100,
-                        value = 500, 
-                        format="#.#permil",
-                        animate=TRUE)
+        # Application headers
+        titlePanel("Isotope labeling calculator"),
+        "This app is intended for use in ", em("estimating"), " incubation times and isotopic enrichments in isotope labeling experiments.",
+        fluidRow(
+            "The source code is available on ",
+            a(href="https://www.github.com/sebkopf/shinyApps#isolabel", "GitHub"),
+            ". Please use the repository's",
+            a(href="https://github.com/sebkopf/shinyApps/issues", "Issue Tracker"),
+            "for any feedback, suggestions and bug reports."
         ),
         
-        conditionalPanel(
-            condition = "input.targetType == 'frac'",
-            sliderInput("intensity_F", 
-                        "", 
-                        min = 0,
-                        max = 1,
-                        step = 0.001,
-                        value = 0.01, 
-                        format="#.#%",
-                        animate=TRUE)
-        )
-    ),
-    
-    # Show a plot of the generated distribution
-    mainPanel(
-        plotOutput("plot")
-    )
+        hr(),
+        
+        sidebarLayout(
+            
+            # side bar panel
+            sidebarPanel(
+                h4("Isotope label"),
+                selectInput("ref", "", 
+                            choices = c("2H", "13C", "15N", "18O", "34S")),
+                htmlOutput("nat"),
+                
+                h5("Label strengths"),
+                sliderInput("label1", "",
+                            min = 0, max = 0.1, step = 0.0001, value = 0.01, 
+                            format="#.##%"),
+                htmlOutput("label1_message"),
+                tags$style(type="text/css", HTML("#label1_message {color: #f50000;}")),
+                sliderInput("label2", "", 
+                            min = 0, max = 0.5, step = 0.0005, value = 0.1, 
+                            format="#.##%"),
+                htmlOutput("label2_message"),
+                tags$style(type="text/css", HTML("#label2_message {color: #f50000;}")),
+                sliderInput("label3", "", 
+                            min = 0, max = 1, step = 0.001, value = 0.5, 
+                            format="#.#%"),
+                htmlOutput("label3_message"),
+                tags$style(type="text/css", HTML("#label3_message {color: #f50000;}")),
+                checkboxGroupInput(inputId="show", label="Show labeling strengths:", choices=1:3, selected=1:3),
+                tags$style(type="text/css", HTML("#show>*{float: left; margin-right: 15px; height: 20px;} #show {height: 20px;}")),
+                
+                h5("Target enrichment"),
+                radioButtons(
+                    "targetType", "",
+                    c(`Relative enrichment [in permil]` = "permil",
+                      `Total abundance [% light isotope]` = "frac"), selected="permil"),
+                
+                conditionalPanel(
+                    condition = "input.targetType == 'permil'",
+                    numericInput("intensity_permil", "", 500, min = 0, step = 100)
+                ),
+                
+                conditionalPanel(
+                    condition = "input.targetType == 'frac'",
+                    sliderInput("intensity_F", "", 
+                                min = 0, max = 1, step = 0.001, value = 0.05, 
+                                format="#.#%", animate=TRUE),
+                    htmlOutput("intensity_F_message"),
+                    tags$style(type="text/css", HTML("#intensity_F_message {color: #f50000;}"))
+                ),
+                
+                h4("Doubling times"),
+                dblt_picker(1, unit = "hour"),
+                dblt_picker(2, unit = "day"),
+                dblt_picker(3, unit = "month"),
+                dblt_picker(4, unit = "year"),
+                dblt_picker(5, value = 10, unit = "year")
+            ),
+            
+            # Main panel / output
+            mainPanel(
+                progressInit(),
+                
+                tabsetPanel(
+                    tabPanel("Plot", 
+                             plotOutput("plot", height="600px"),
+                             downloadButton('downloadPlot', 'Download Plot')), 
+                    tabPanel("Table", 
+                             h4("Enrichment of different doubling times as function of label strength and incubation time"), 
+                             radioButtons(
+                                 "tableDataType", "Report data in:",
+                                 c(`Enrichment [in permil]` = "permil",
+                                   `Total abundance [% light isotope]` = "frac"), selected="permil"),
+                             tableOutput("table"),
+                             downloadButton('downloadTable', 'Download Table')),
+                    selected = "Table")
+            ), 
+            
+            position = "left"
+        ),        
+    div("Written by ", a(href="mailto:skopf@caltech.edu", "Sebastian Kopf"), br(), 
+        "Powered by ", a(href="http://www.rstudio.com/", "RStudio"), "'s' ", a(href="http://www.rstudio.com/shiny/", "Shiny engine"), br(),
+        "Comes with absolutely no warranty", br(),
+        "Released under ", a(href="http://www.gnu.org/licenses/gpl-3.0.html", "GPL-3"), align="right")
 ))
