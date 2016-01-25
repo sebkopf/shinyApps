a0c7110b8bac540565abba4fe1120e089eed77ae
Sebastian Kopf <seb.kopf@gmail.com>
2016-01-24 19:26:21 -0500

    include nitrogen cycle dynamics in apps overview

--- run_apps.R
+++ run_apps.R
@@ -7,3 +7,5 @@ run_app <- function(name, ...) {
 isolabel <- function() run_app("isolabel")
 dynamic <- function() run_app("dynamic")
 sdv <- function() run_app("sigman_data_viewer", host = "0.0.0.0", port = 1234)
+isozoom <- function() run_app("example_isoread_zoom")
+ncycle <- function() run_app("nitrogen_cycle_dynamics")
