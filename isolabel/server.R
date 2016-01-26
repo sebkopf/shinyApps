99fa927972d3eacb542c5b06bdda83655b9a18ef
Sebastian Kopf <seb.kopf@gmail.com>
2016-01-26 12:10:50 -0500

    calculation status bar update

--- isolabel/server.R
+++ isolabel/server.R
@@ -55,82 +55,89 @@ shinyServer(function(input, output, session) {
     
     
     # Main inputs processing ------------------------------------------------
-    observe({
-      input$updatePlot1 # update button pressed
-      input$updatePlot2 # update button pressed
-      input$updateTable # update button pressed
-      input$tableDataType # simple notation trigger also instantaneous
-      input$plot2DataType # simple notation trigger also instantaneous
-      
-      isolate({
-        data$plot1.df <- NULL
-        data$plot2.df <- NULL
-        data$table.df <- NULL
+    observe(
+      withProgress(session, min=1, max=5, {
+        setProgress(message = 'Calculating ...')
+        setProgress(value = 2)
         
-        # checks
-        if (data$target.ab <= to_ab(data$nat)) return() # problem
-        if (nrow(get_iso_labels()) == 0) return() # no label is strong enough or excluded because of error
-        if (nrow(get_doubling_times()) == 0) return() # no doubling times selected
+        input$updatePlot1 # update button pressed
+        input$updatePlot2 # update button pressed
+        input$updateTable # update button pressed
+        input$tableDataType # simple notation trigger also instantaneous
+        input$plot2DataType # simple notation trigger also instantaneous
         
-        # plot 1 data (labeling times)
-        data$plot1.df <- 
-          label_time(
-            dblt = get_doubling_times()$dblt, 
-            target = data$target.ab, 
-            tracer = get_iso_labels()$effective.wab,
-            natural = data$nat) %>% 
-          merge(get_iso_labels(), by.x = "tracer.ab", by.y = "effective.wab") %>% 
-          mutate(Label = factor(Label, levels = get_iso_labels()$Label))
-
-        # plot 2 data (enrichment curves)
-        times <- seq(0, max(data$plot1.df$labeling_time) * input$plot2Xzoom/100, length.out = 50)
-        data$plot2.df <- 
-          label_strength(
-            time = duration(c(1, times), "seconds"), 
-            dblt = get_doubling_times()$dblt, 
-            tracer = get_iso_labels()$effective.wab,
-            natural = data$nat) %>% 
-          merge(get_doubling_times(), by = "dblt") %>% 
-          merge(get_iso_labels(), by.x = "tracer.ab", by.y = "effective.wab") %>% 
-          mutate(
-            DBLT = factor(label, levels = get_doubling_times()$label),
-            Spike = factor(Label, levels = get_iso_labels()$Label),
-            enrichment = get_value(if(input$plot2DataType == "permil") total.delta else total.ab)
-          )
-        
-        # table 1 data
-        data$table.df <- 
-          get_iso_labels()$effective.wab %>% 
-          # calculate the labeling curves for each tracer
-          lapply(function(i){
-            df <- subset(data$plot1.df, tracer.ab == i)
+        isolate({
+          data$plot1.df <- NULL
+          data$plot2.df <- NULL
+          data$table.df <- NULL
+          
+          # checks
+          if (data$target.ab <= to_ab(data$nat)) return() # problem
+          if (nrow(get_iso_labels()) == 0) return() # no label is strong enough or excluded because of error
+          if (nrow(get_doubling_times()) == 0) return() # no doubling times selected
+          
+          # plot 1 data (labeling times)
+          data$plot1.df <- 
+            label_time(
+              dblt = get_doubling_times()$dblt, 
+              target = data$target.ab, 
+              tracer = get_iso_labels()$effective.wab,
+              natural = data$nat) %>% 
+            merge(get_iso_labels(), by.x = "tracer.ab", by.y = "effective.wab") %>% 
+            mutate(Label = factor(Label, levels = get_iso_labels()$Label))
+          setProgress(value = 3)
+          
+          # plot 2 data (enrichment curves)
+          times <- seq(0, max(data$plot1.df$labeling_time) * input$plot2Xzoom/100, length.out = 50)
+          data$plot2.df <- 
             label_strength(
-              time = duration(df$labeling_time, "seconds"), 
-              dblt = duration(df$dblt, "seconds"), 
-              tracer = abundance(i) %>% set_attrib(minor = data$nat@isoname, major = data$nat@major),
-              natural = data$nat)
-          }) %>% 
-          dplyr::bind_rows() %>% 
-          # merge in doubling times and iso labels
-          merge(get_doubling_times() %>% mutate(label = label %>% paste("doubling")), by = "dblt") %>% 
-          merge(get_iso_labels(), by.x = "tracer.ab", by.y = "effective.wab") %>% 
-          mutate(
-            `Incubation time [s]` = as.numeric(time), 
-            `Incubation time` = time.label,
-            enrichment = 
-              if (input$tableDataType == "permil") {
-                total.delta %>% get_value() %>% signif(3) %>% paste()
-              } else {
-                total.ab %>% get_value() %>% multiply_by(100) %>% signif(3) %>% paste()
-              }) %>% 
-          # cast into wide format
-          dcast(`Label` + `Incubation time [s]` + `Incubation time` ~ label, value.var = "enrichment")
-        
-        # header order
-        header_order <- match(paste(get_doubling_times()$label, "doubling"), names(data$table.df))
-        data$table.df <- data$table.df[c(1,3,header_order)]
-      })
-    })
+              time = duration(c(1, times), "seconds"), 
+              dblt = get_doubling_times()$dblt, 
+              tracer = get_iso_labels()$effective.wab,
+              natural = data$nat) %>% 
+            merge(get_doubling_times(), by = "dblt") %>% 
+            merge(get_iso_labels(), by.x = "tracer.ab", by.y = "effective.wab") %>% 
+            mutate(
+              DBLT = factor(label, levels = get_doubling_times()$label),
+              Spike = factor(Label, levels = get_iso_labels()$Label),
+              enrichment = get_value(if(input$plot2DataType == "permil") total.delta else total.ab)
+            )
+          setProgress(value = 4)
+          
+          # table 1 data
+          data$table.df <- 
+            get_iso_labels()$effective.wab %>% 
+            # calculate the labeling curves for each tracer
+            lapply(function(i){
+              df <- subset(data$plot1.df, tracer.ab == i)
+              label_strength(
+                time = duration(df$labeling_time, "seconds"), 
+                dblt = duration(df$dblt, "seconds"), 
+                tracer = abundance(i) %>% set_attrib(minor = data$nat@isoname, major = data$nat@major),
+                natural = data$nat)
+            }) %>% 
+            dplyr::bind_rows() %>% 
+            # merge in doubling times and iso labels
+            merge(get_doubling_times() %>% mutate(label = label %>% paste("doubling")), by = "dblt") %>% 
+            merge(get_iso_labels(), by.x = "tracer.ab", by.y = "effective.wab") %>% 
+            mutate(
+              `Incubation time [s]` = as.numeric(time), 
+              `Incubation time` = time.label,
+              enrichment = 
+                if (input$tableDataType == "permil") {
+                  total.delta %>% get_value() %>% signif(3) %>% paste()
+                } else {
+                  total.ab %>% get_value() %>% multiply_by(100) %>% signif(3) %>% paste()
+                }) %>% 
+            # cast into wide format
+            dcast(`Label` + `Incubation time [s]` + `Incubation time` ~ label, value.var = "enrichment")
+          
+          # header order
+          header_order <- match(paste(get_doubling_times()$label, "doubling"), names(data$table.df))
+          data$table.df <- data$table.df[c(1,3,header_order)]
+          setProgress(value = 5)
+        })
+      }))
     
     
     # Plots ------------------------------------------------
